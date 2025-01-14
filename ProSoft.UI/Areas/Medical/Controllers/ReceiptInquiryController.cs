c2d3cc1dfb6399574f0d2ccf2ec32b7691306011
aymanprocessor <aymansaadhack@gmail.com>
2025-01-14 11:11:48 +0200

    Add Receipt Inquiry feature with localization support and UI enhancements

---
+++ ProSoft.UI/Areas/Medical/Controllers/ReceiptInquiryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProSoft.UI.Areas.Medical.Controllers
+{
+    [Authorize]
+    [Area(nameof(Medical))]
+    public class ReceiptInquiryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
