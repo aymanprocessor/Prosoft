feff269de677a1894625c36f2a54b90b5e7d87c9
aymanprocessor <aymansaadhack@gmail.com>
2025-05-19 17:17:58 +0300

    checkpoint 19-5-2025

--- Shared/Global/GlobalConstants.cs
+++ Shared/Global/GlobalConstants.cs
@@ -6,6 +6,7 @@
         public const string InsertIcon = "<i class=\"bi bi-plus-circle\"></i>";
         public const string EditIcon = "<i class=\"bi bi-pencil\"></i>";
         public const string DeleteIcon = "<i class=\"bi bi-trash\"></i>";
+        public const string SaveIcon = "<i class=\"bi bi-floppy\"></i>";
         public const string FormatDate = "yyyy-MM-dd";
         public const string FormatTime = "hh:MM tt";
     }
